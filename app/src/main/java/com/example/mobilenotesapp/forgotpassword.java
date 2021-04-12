4470159f36468912a16cc9f8eb4c7726a6206082
WAJIDSAIN <78271811+WAJIDSAIN@users.noreply.github.com>
2021-04-12 12:14:50 +0530

    Secure NotesApp

---
+++ app/src/main/java/com/example/mobilenotesapp/forgotpassword.java
@@ -0,0 +1,84 @@
+package com.example.mobilenotesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class forgotpassword extends AppCompatActivity {
+
+    private EditText mforgotpassword;
+    private Button mpasswordrecoverbutton;
+    private TextView mgobacktologin;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotpassword);
+
+
+        getSupportActionBar().hide();
+
+        mforgotpassword=findViewById(R.id.forgotpassword);
+        mpasswordrecoverbutton=findViewById(R.id.passwordrecoverbutton);
+        mgobacktologin=findViewById(R.id.gobacktologin);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+
+        mgobacktologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(forgotpassword.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mpasswordrecoverbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=mforgotpassword.getText().toString().trim();
+                if(mail.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Enter your Email", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //we have to send password mail
+
+                    firebaseAuth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(getApplicationContext(),"Mail Sent, You Can Recover Your Password Using Mail",Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(forgotpassword.this,MainActivity.class));
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(),"Email is Invalid OR Account Not Exist ",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
